1b822a95102ec3c40be4fbbe1d87bcabcebaf025
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-04-20 08:37:04 -0600

    tried to fix cors issue but failed

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,19 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000',
+	uri: 'http://localhost:4000/',
+	// uri: 'https://movie-ql.now.sh',
+	// headers: {
+	// 	'Content-Type': 'application/json',
+	// 	'Access-Control-Allow-Origin': '*',
+	// 	'Access-Control-Allow-Methods':
+	// 		'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+	// 	'Access-Control-Allow-Headers': '*',
+	// 	'Access-Control-Allow-Credentials': true,
+	// },
+	// fetchOptions: {
+	// 	mode: 'no-cors',
+	// },
 	resolvers: {
 		Movie: {
 			isLiked: () => false,
